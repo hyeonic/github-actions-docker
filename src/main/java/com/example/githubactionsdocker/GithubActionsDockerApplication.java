d1f47fe5768bb18a61a2b9c2017b03d8bbf50116
hyeonic <dev.hyeonic@gmail.com>
2021-09-30 15:30:51 +0900

    feat: project 초기 설정 추가

---
+++ src/main/java/com/example/githubactionsdocker/GithubActionsDockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubactionsdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionsDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/githubactionsdocker/GithubActionsDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubactionsdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
