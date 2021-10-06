6ea35b6bbfbac4157fa8fe3c41c61d01f1f73e48
hyeonic <dev.hyeonic@gmail.com>
2021-10-06 20:16:21 +0900

    feat: build.gradle 추가

---
+++ src/test/java/me/hyeonic/githubactionsdocker/web/IndexControllerTest.java
@@ -0,0 +1,25 @@
+package me.hyeonic.githubactionsdocker.web;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+class IndexControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("GET /")
+    void defaultPage() throws Exception {
+
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk());
+    }
+}
